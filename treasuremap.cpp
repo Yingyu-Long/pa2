256b1cac31095258a3e7cb6a75b80fe9eb335a7e
Yingyu-Long <albertlong66@outlook.com>
2026-03-13 15:03:25 -0700

    BFS implemented

--- treasuremap.cpp
+++ treasuremap.cpp
@@ -34,14 +34,58 @@ void TreasureMap::SetLOB(PNG& im, pair<int, int> loc, int d) {
 }
 
 PNG TreasureMap::RenderMap() {
-	/* REPLACE THE LINE BELOW WITH YOUR CODE */
+	vector<vector<bool>> visited(base.height(), vector<bool>(base.width(), false));
+	vector<vector<int>> distance(base.height(), vector<int>(base.width(), 0));
+	Queue <pair<int, int>> q;
+	visited[start.second][start.first] = true;
+	distance[start.second][start.first] = 0;
+	while(!q.IsEmpty()){
+		pair<int, int> current = q.Dequeue();
+		vector<pair<int, int>> neighbors = Neighbours(current);
+		for(pair<int, int> neighbor : neighbors) {
+			if(Good(visited, current, neighbor)) {
+				visited[neighbor.second][neighbor.first] = true;
+				distance[neighbor.second][neighbor.first] = distance[current.second][current.first] + 1;
+				SetLOB(base, neighbor, distance[neighbor.second][neighbor.first]);
+				q.Enqueue(neighbor);
+			}
+		}
+	}
 	return PNG();
 }
 
 
 PNG TreasureMap::RenderMaze() {
-	/* REPLACE THE LINE BELOW WITH YOUR CODE */
-	return PNG();
+	PNG base_copy = base;
+	for (int dx = -3; dx <= 3; dx++) {
+		for (int dy = -3; dy <= 3; dy++) {
+			int x = start.first + dx;
+			int y = start.second + dy;
+			if(x >=0 && x <base_copy.width() && y >= 0 && y < base_copy.height()) {
+				RGBAPixel* pixel = base_copy.getPixel(x, y);
+				pixel->r = 255;
+				pixel->g = 0;
+				pixel->b = 0;
+			}
+		}
+	}
+	vector<vector<bool>> visited(base_copy.height(), vector<bool>(base_copy.width(), false));
+	Queue <pair<int, int>> q;
+	visited[start.second][start.first] = true;
+	SetGrey(base_copy, start);
+	q.Enqueue(start);
+	while(!q.IsEmpty()) {
+		pair<int, int> current = q.Dequeue();
+		vector<pair<int, int>> neighbors = Neighbours(current);
+		for(pair<int, int> neighbor : neighbors) {
+			if(Good(visited, current, neighbor)) {
+				visited[neighbor.second][neighbor.first] = true;
+				SetGrey(base_copy, neighbor);
+				q.Enqueue(neighbor);
+			}
+		}
+	}
+	return base_copy;
 }
 
 bool TreasureMap::Good(vector<vector<bool>>& v, pair<int, int> curr, pair<int, int> next) {
@@ -50,7 +94,7 @@ bool TreasureMap::Good(vector<vector<bool>>& v, pair<int, int> curr, pair<int, i
 	if (next.second < 0 || next.second >= maze.height()) return false;
 
 	// unvisited
-	if(v[next.first][next.second]) return false;
+	if(v[next.second][next.first]) return false;
 	
 	// same colour as curr in the maze image
 	if(maze.getPixel(next.first, next.second) != maze.getPixel(curr.first, curr.second)) return false;
@@ -59,8 +103,11 @@ bool TreasureMap::Good(vector<vector<bool>>& v, pair<int, int> curr, pair<int, i
 }
 
 vector<pair<int, int>> TreasureMap::Neighbours(pair<int, int> curr) {
-	/* REPLACE THE LINES BELOW WITH YOUR CODE */
 	vector<pair<int, int>> v;
+	v.push_back(make_pair(curr.first-1, curr.second));
+	v.push_back(make_pair(curr.first, curr.second+1));
+	v.push_back(make_pair(curr.first+1, curr.second));
+	v.push_back(make_pair(curr.first, curr.second-1));
 	return v;
 }
 
